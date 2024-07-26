7f23de367a09261dc24f14460bc6678253d0c668
Zijian Huang <47173434+tkwongspace@users.noreply.github.com>
2024-07-26 16:48:28 +0800

    Update codes to run on Raspberry Pi
    In situ debugging on Raspberry Pi

--- src/get_warnings_for_sql.py
+++ src/get_warnings_for_sql.py
@@ -1,68 +1,89 @@
 # Save this as get_weather_warning.py
 import pymysql
-import requests
-
+import time
 from toolbox import *
 from private import my_apikey, my_sql_ip, my_sql_user, my_sql_password
 
 
 # set up logging
-set_up_logging(file=r'../record/weather_log.txt')
+set_up_logging(file=r'./record/weather_log.txt')
+
+# local backup record
+local_bkp = r"./record/weather_data.json"
 
 # parameters for api connection
 api_key = my_apikey()
 base_url = 'https://devapi.qweather.com/v7/warning/now?'
-location_code = '101310215'  # City code: github.com/qwd/LocationList/blob/master/China-City-List-latest.csv
+# City code: github.com/qwd/LocationList/blob/master/China-City-List-latest.csv
+location_code = '101280108'  # Haizhu 101280108 | Panyu 101280102 | Futian 101280603 | Xiangzhou 101280704
 complete_url = base_url + 'location=' + location_code + '&lang=en&key=' + api_key
 
-# MariaDB connection
-db = pymysql.connect(
-    host=my_sql_ip(),
-    user=my_sql_user(),
-    password=my_sql_password(),
-    database="weather_warnings_db"
-)
-cursor = db.cursor()
+# define MariaDB (MySQL) connection details
+db_config = {
+    'user': my_sql_user(),
+    'password': my_sql_password(),
+    'host': my_sql_ip(),
+    'database': 'weather_warnings_db'
+}
 
-# restore previous record
-pre_id = get_previous_record("../record/weather_data.json")
 
-# Fetch weather warning data
-try:
-    response = requests.get(complete_url)
-    response.raise_for_status()  # raise an exception for bad status codes
+def main():
+    while True:
+        # call API to download weather warnings and backup
+        # and arrange the downloaded information
+        weather_data = get_warning_info(complete_url, local_bkp)        
 
-    data = response.json()
-    warnings = data['warning']
-    if warnings:
-        current_warnings = read_warnings(warnings)
-        # arrange warning information for database
-        warnings_id = current_warnings['id']
-        warnings_status = current_warnings['status']
-        warnings_type = current_warnings['typeName']
-        warnings_color = current_warnings['color']
-        warnings_issue_time = current_warnings['time']
-        # save the warning information
-        with open("../record/weather_data.json", "w") as outfile:
-            json.dump(data, outfile)
-    else:
-        warnings_id = None
-        warnings_status = None
-        warnings_type = None
-        warnings_color = None
-        warnings_issue_time = None
-except requests.exceptions.RequestException as e:
-    logging.error(f">! Error fetching warning data: {e}.")
+        # connect to database
+        try:
+            connection = pymysql.connect(
+                user=db_config['user'],
+                password=db_config['password'],
+                host=db_config['host'],
+                database=db_config['database']
+            )            
+            # store_data_in_database(weather_data, connection)
+            cursor = connection.cursor()
 
-# Process and insert data into database
-query = "INSERT INTO warnings (id, status, time, typename, color) VALUES (%s, %s, %s, %s, %s)"
+            # delete previous warning information
+            cursor.execute("DELETE FROM warnings WHERE tag='previous'")
 
-for i in range(len(warnings_id)):
-    values = (warnings_id[i], warnings_status[i], warnings_issue_time[i], warnings_type[i], warnings_color[i])
-    cursor.execute(query, values)
+            # update the tag of the 'current' to 'previous'
+            cursor.execute("UPDATE warnings SET tag='previous' WHERE tag='current'")
 
-db.commit()
+            # insert new data as current
+            if not weather_data:
+                cursor.execute("""
+                    INSERT INTO warnings (id, status, time, typename, color, tag, mark)
+                    VALUES (NULL, NULL, NULL, NULL, NULL, 'current', NULL)
+                """)
+            else:
+                for i in range(len(weather_data['id'])):
+                    id = weather_data['id'][i]
+                    status = weather_data['status'][i]
+                    issue_time = weather_data['time'][i]
+                    name = weather_data['typeName'][i]
+                    color = weather_data['color'][i]
+                    mark = weather_data['tag'][i]
+                    cursor.execute("""
+                        INSERT INTO warnings (id, status, time, typename, color, tag, mark)
+                        VALUES (%s, %s, %s, %s, %s, 'current', %s)
+                    """, (id, status, issue_time, name, color, mark))
+            
+            connection.commit()
 
-# Close the connection
-cursor.close()
-db.close()
+            print(">> Success in fetching warning info.")
+            print("  Update in 90 seconds...")
+
+        except Exception as e:
+            print(f">! Error in fetching warning info: {e}.")
+            print("   Retry in 90 seconds...")
+
+        finally:
+            cursor.close()
+            connection.close()
+
+        time.sleep(90)
+
+
+if __name__ == "__main__":
+    main()
--- src/toolbox.py
+++ src/toolbox.py
@@ -1,5 +1,17 @@
 import json
 import logging
+import requests
+
+from datetime import datetime
+
+
+# def connect_to_mysql(sql_user, sql_pw, sql_host, sql_db):
+#     return pymysql.connect(
+#         user=sql_user,
+#         password=sql_pw,
+#         host=sql_host,
+#         database=sql_db
+#     )
 
 
 def find_duplicate_warnings(name_list, color_list):
@@ -33,10 +45,39 @@ def get_previous_record(file):
                 # previous_color = [w['severityColor'] for w in previous_response['warning']]
                 # previous_level = [w['level'] for w in previous_response['warning']]
                 # previous_time = [w['startTime'] for w in previous_response['warning']]
+            else:
+                previous_id = []
             return previous_id
 
     except FileNotFoundError:
         print(">> Previous record not found..")
+
+
+def get_warning_info(url, backup_record):
+    # get previous ID stored in local file
+    previous_id = get_previous_record(backup_record)   
+
+    # call the API
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        warnings = data['warning']        
+        
+        # backup warning info at local file
+        with open(backup_record, "w") as f:
+            json.dump(data, f)
+
+        if warnings:
+            # create dictionary for weather info
+            current_warnings = read_warnings(warnings, previous_id)
+        else:
+            current_warnings = {}
+        
+        return current_warnings
+    
+    except requests.exceptions.RequestException as e:
+        logging.error(f">! Error fetching warning data: {e}.")
 
 
 # define the priority levels
@@ -61,6 +102,7 @@ def read_warnings(warnings, previous_warning_ids):
     warnings_color = [w['severityColor'] for w in warnings]
     warnings_level = [w['level'] for w in warnings]  # FutureWarning: Will be aborted in short time.
     warnings_time = [w['startTime'] for w in warnings]
+    warnings_time_formatted = [datetime.strptime(t, '%Y-%m-%dT%H:%M%z').strftime('%Y-%m-%d %H:%M') for t in warnings_time]
 
     # filter warnings with priority check
     warning_information = ['tag', 'id', 'typeName', 'status', 'color', 'time']
@@ -82,14 +124,14 @@ def read_warnings(warnings, previous_warning_ids):
                 # replace thw lower priority item
                 current['id'][duplicate_index] = warnings_id[i]
                 current['status'][duplicate_index] = warnings_status[i]
-                current['time'][duplicate_index] = warnings_time[i]
+                current['time'][duplicate_index] = warnings_time_formatted[i]
                 current['typeName'][duplicate_index] = warnings_name[i]
                 current['color'][duplicate_index] = warning_color
         else:
             # add the item if it's not seen yet
             current['id'].append(warnings_id[i])
             current['status'].append(warnings_status[i])
-            current['time'].append(warnings_time[i])
+            current['time'].append(warnings_time_formatted[i])
             current['typeName'].append(warnings_name[i])
             current['color'].append(warning_color)
 
@@ -104,4 +146,94 @@ def set_up_logging(file):
     )
 
 
+def store_data_in_database(data, connection):
+    cursor = connection.cursor()
 
+    # delete previous warning information
+    cursor.execute("DELETE FROM warnings WHERE tag='previous'")
+
+    # update the tag of the 'current' to 'previous'
+    cursor.execute("UPDATE warnings SET tag='previous' WHERE tag='current'")
+
+    # insert new data as current
+    if not data:
+        cursor.execute("""
+            INSERT INTO warnings (id, status, time, typename, color, tag, mark)
+            VALUES (NULL, NULL, NULL, NULL, NULL, 'current', NULL)
+        """)
+    else:
+        for i in range(len(data['id'])):
+            cursor.execute("""
+                INSERT INTO warnings (id, status, time, typename, color, tag, mark)
+                VALUES (%s, %s, %s, %s, %s, 'current', %s)
+            """, 
+            (data['id'][i], data['status'][i], data['time'][i], 
+             data['typeName'][i], data['color'][i], data['tag'][i]))
+    
+        # # compare warning ID to identify maintained / newly issued warnings
+        # cursor.execute("""
+        #     SELECT a.id FROM warnings a
+        #     JOIN warnings b
+        #     ON a.id = b.id
+        #     WHERE a.tag='current' AND b.tag='previous'
+        # """)
+        # maintained_ids = [row[0] for row in cursor.fetchall()]
+
+        # cursor.execute("SELECT id FROM warnings WHERE state='current'")
+        # current_ids = [row[0] for row in cursor.fetchall()]
+
+        # # update the mark with 'maintained' or 'new'
+        # for wid in current_ids:
+        #     mark = 'maintained' if wid in maintained_ids else 'new'
+        #     cursor.execute("""
+        #         UPDATE warnings SET mark=%s WHERE id=%s AND tag='current'
+        #     """, (mark, wid))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return
+
+# [ARCHIVE CODES]
+# # restore previous record
+# pre_id = get_previous_record("../record/weather_data.json")
+
+# # Fetch weather warning data
+# try:
+#     response = requests.get(complete_url)
+#     response.raise_for_status()  # raise an exception for bad status codes
+
+#     data = response.json()
+#     warnings = data['warning']
+#     if warnings:
+#         current_warnings = read_warnings(warnings)
+#         # arrange warning information for database
+#         warnings_id = current_warnings['id']
+#         warnings_status = current_warnings['status']
+#         warnings_type = current_warnings['typeName']
+#         warnings_color = current_warnings['color']
+#         warnings_issue_time = current_warnings['time']
+#         # save the warning information
+#         with open("../record/weather_data.json", "w") as outfile:
+#             json.dump(data, outfile)
+#     else:
+#         warnings_id = None
+#         warnings_status = None
+#         warnings_type = None
+#         warnings_color = None
+#         warnings_issue_time = None
+
+
+# # Process and insert data into database
+# query = "INSERT INTO warnings (id, status, time, typename, color) VALUES (%s, %s, %s, %s, %s)"
+
+# for i in range(len(warnings_id)):
+#     values = (warnings_id[i], warnings_status[i], warnings_issue_time[i], warnings_type[i], warnings_color[i])
+#     cursor.execute(query, values)
+
+# db.commit()
+
+# # Close the connection
+# cursor.close()
+# db.close()
