f6b11ae084a59d54bac4abc4617c383d536a8512
Zijian Huang <47173434+tkwongspace@users.noreply.github.com>
2024-07-26 21:23:10 +0800

    Add uWSGI service

--- src/flask_api.py
+++ src/flask_api.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import logging
+# import logging
 import pymysql
 
 from private import my_sql_ip, my_sql_user, my_sql_password
@@ -8,13 +8,14 @@ from private import my_sql_ip, my_sql_user, my_sql_password
 app = Flask(__name__)
 
 
+# # Enable logging
+# logging.basicConfig(level=logging.DEBUG)
+
 # Database connection details
 db_host = my_sql_ip()
 db_user = my_sql_user()
 db_password = my_sql_password()
 db_name = 'weather_warnings_db'
-
-logging.basicConfig(level=logging.DEBUG)
 
 
 def get_db_connection():
@@ -29,9 +30,9 @@ def get_db_connection():
     return connection
 
 
-@app.route('/weather_warning', methods=['GET'])
+@app.route('/current_warning', methods=['GET'])
 def get_latest_warning():
-    logging.debug("Entering weather_warning endpoint")
+    # logging.debug("Entering weather_warning endpoint")
     try:
         # MariaDB connection
         connection = get_db_connection()
@@ -39,8 +40,10 @@ def get_latest_warning():
 
         # Get the latest warning
         query = "SELECT * FROM warnings WHERE tag = 'current'"
+        # logging.debug(f"Executing query: {query}")
         cursor.execute(query)
         result = cursor.fetchall()
+        # logging.debug(f"Query result: {result}")
 
         # Close the connection
         cursor.close()
@@ -49,9 +52,9 @@ def get_latest_warning():
         return jsonify(result)
 
     except Exception as e:
-        logging.error(f"An error occurred: {e}.")
+        # logging.error(f"An error occurred: {e}.")
         return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
